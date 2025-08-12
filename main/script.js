443b2b2faef59a7bba6d7c5aa0bddcfe3ba4a681
Xavier Rodrigues <xavierrodrigues2018x@gmail.com>
2025-08-12 11:40:34 +0100

    1

--- main/script.js
+++ main/script.js
@@ -5,5 +5,12 @@ function loadPage(page) {
       document.getElementById('content').innerHTML = html;
     });
 }
+
+function setActive(element) {
+  const links = document.querySelectorAll('nav ul li a');
+  links.forEach(link => link.classList.remove('active'));
+  element.classList.add('active');
+}
+
 // Load default page on start
 loadPage('home.html');
